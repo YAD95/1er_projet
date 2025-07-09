76607abc76f7351772e1f6d6ad9fc0b5e24ff20e
YAD95 <danielalican.yilmaz@gmail.com>
2025-07-09 20:00:24 +0200

    Premier commit

---
+++ 1er_projet.py
@@ -0,0 +1,3 @@
+nombre1 = int(input("""Veuillez entrer un premier nombre : """))
+nombre2 = int(input("""Veuillez entrer un deuxième nombre : """))
+print(f"""Le résultat de l'addition du nombre {nombre1} avec le nombre {nombre2} est égal à {nombre1+nombre2}""")
